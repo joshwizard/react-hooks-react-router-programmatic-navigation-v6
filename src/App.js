144076ee5fb96229268636da093232ac24c9517c
joshwizard <murikij@gmail.com>
2025-10-27 15:15:56 +0300

    Added code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { useEffect, useState } from "react";
 
 function App() {
 
+// Add code to mock user authentication
+const [isLoggedIn, setIsLoggedIn] = useState(false)
+const navigate = useNavigate()
+
+const login = () => {
+  setIsLoggedIn(true)
+}
+
+const logout = () => {
+  setIsLoggedIn(false)
+}
+
+// Add programmatic navigation for login and logout
+useEffect(() => {
+  if(isLoggedIn) {
+    // navigates to Home route if user is logged in
+    navigate("/")
+  } else {
+    // navigate to Login route if user is logged out
+    navigate("/login")
+  }
+}, [isLoggedIn])
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      {/* Add conditional rendering so users have to be logged in to see pages on the site */}
+      {isLoggedIn ? <NavBar logout={logout}/> : <Navigate to={"/login"} />}
+      <Outlet context={login}/>
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 function Login() {
+  const login = useOutletContext()
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -13,8 +15,10 @@ function Login() {
     });
   }
 
+  // Create a function that calls the login function when the form is submitted
   function handleLogin(e) {
     e.preventDefault();
+    login()
   }
 
   return (
